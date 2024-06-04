7a0090157d93d404120adfa3cdcdfe8a7080e5de
BotNicholas <botannicolai22@gmail.com>
2024-06-04 11:41:08 +0300

    Simple MyBatis implementation WITH mapper interfaces

--- src/main/java/org/nicholas/Main.java
+++ src/main/java/org/nicholas/Main.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.nicholas.mappers.StudentMapper;
 import org.nicholas.models.Student;
 
 import java.io.IOException;
@@ -11,15 +12,28 @@ import java.io.Reader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml"); //Resources class makes it easier to load different resources from classpath and other resources
+//        Without Mapper Interface
+//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml"); //Resources class makes it easier to load different resources from classpath and other resources
+//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+//        SqlSession session = sessionFactory.openSession();
+//
+//        Student student = new Student(0, "Mohammad", "It", 80, 984803322, "Mohamad@gmail.com");
+//
+//        session.insert("StudentNamespace.insert", student);
+////        session.insert("insert", student);
+//        System.out.println("Record has been inserted successfully!");
+//        session.commit();
+//        session.close();
+
+//        With mapper interface
+        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession session = sessionFactory.openSession();
+        StudentMapper mapper = session.getMapper(StudentMapper.class);
 
         Student student = new Student(0, "Mohammad", "It", 80, 984803322, "Mohamad@gmail.com");
+        mapper.insert(student);
 
-        session.insert("StudentNamespace.insert", student);
-//        session.insert("insert", student);
-        System.out.println("Record has been inserted successfully!");
         session.commit();
         session.close();
     }
---
+++ src/main/java/org/nicholas/mappers/StudentMapper.java
@@ -0,0 +1,7 @@
+package org.nicholas.mappers;
+
+import org.nicholas.models.Student;
+
+public interface StudentMapper {
+    void insert(Student student);
+}
