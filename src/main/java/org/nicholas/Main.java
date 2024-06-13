15c6e0c7b6ec12801aa5b8750e1b088b736a6d4a
BotNicholas <botannicolai22@gmail.com>
2024-06-13 14:46:53 +0300

    Annotation approach with mapper interface inclusion in mybatis/SqlMapConfig.xml

--- src/main/java/org/nicholas/Main.java
+++ src/main/java/org/nicholas/Main.java
@@ -13,128 +13,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//        Without Mapper Interface
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml"); //Resources class makes it easier to load different resources from classpath and other resources
-//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession session = sessionFactory.openSession();
-//
-//        Student student = new Student(0, "Mohammad", "It", 80, 984803322, "Mohamad@gmail.com");
-//
-//        session.insert("StudentNamespace.insert", student);
-////        session.insert("insert", student);
-//        System.out.println("Record has been inserted successfully!");
-//        session.commit();
-//        session.close();
-
-
-//        With mapper interface
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession session = sessionFactory.openSession();
-//        StudentMapper mapper = session.getMapper(StudentMapper.class);
-//
-//        Student student = new Student(0, "Mohammad", "It", 80, 984803322, "Mohamad@gmail.com");
-//        mapper.insert(student);
-//
-//        session.commit();
-//        session.close();
-
-
-//        SELECT WITHOUT MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession session = sessionFactory.openSession();
-//
-//        List<Student> students = session.selectList("StudentNamespace.getAll");
-//
-//        students.forEach(System.out::println);
-//        System.out.println();
-//
-//        Student student = session.selectOne("StudentNamespace.getById", 1);
-//
-//        System.out.println(student);
-//
-//        session.commit();
-//        session.close();
-
-//        SELECT WITH MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession session = sessionFactory.openSession();
-//        StudentMapper mapper = session.getMapper(StudentMapper.class);
-//
-//        mapper.getAll().forEach(System.out::println);
-//        System.out.println();
-//
-//        System.out.println(mapper.getById(1));
-
-
-
-//        UPDATE WITHOUT MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//
-//        Student student = sqlSession.selectOne("StudentNamespace.getById", 1);
-//        System.out.println("Current student: " + student);
-//
-//        student.setName(student.getName() + "_COPY");
-//
-//        sqlSession.update("StudentNamespace.update", student);
-//        sqlSession.commit();
-////        sqlSession.close();
-//
-//        Student newStudent = sqlSession.selectOne("StudentNamespace.getById", 1);
-//        System.out.println("New student: " + newStudent);
-//        sqlSession.commit();
-//        sqlSession.close();
-
-//        UPDATE WITH MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
-//
-//        Student student = studentMapper.getById(1);
-//        System.out.println("Current student: " + student);
-//
-//        student.setName(student.getName() + "_COPY");
-//
-//        studentMapper.update(student);
-////        sqlSession.commit();
-////        sqlSession.close();
-//
-//        Student newStudent = studentMapper.getById(1);
-//        System.out.println("New student: " + newStudent);
-//        sqlSession.commit();
-//        sqlSession.close();
-
-
-
-//        DELETE WITHOUT MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//
-//        sqlSession.delete("StudentNamespace.delete", 1);
-//        sqlSession.commit();
-//        sqlSession.close();
-
-//        DELETE WITH MAPPER INTERFACE
-//        Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
-//        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-//        SqlSession sqlSession = sqlSessionFactory.openSession();
-//        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
-//
-//        studentMapper.delete(2);
-//
-//        sqlSession.commit();
-//        sqlSession.close();
-
+        
         Reader reader = Resources.getResourceAsReader("mybatis/SqlMapConfig.xml");
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
         SqlSession session = sessionFactory.openSession();
-        session.getConfiguration().addMapper(StudentMapper.class);
 
         StudentMapper mapper = session.getMapper(StudentMapper.class);
 
@@ -151,16 +33,3 @@ public class Main {
         session.close();
     }
 }
-
-
-
-//Multiple parameters in query
-//
-//<select id="findUsersByNameAndEmail" resultType="User">
-//    SELECT * FROM users WHERE name = #{name} AND email = #{email}
-//</select>
-//...
-//Map<String, Object> params = new HashMap<>();
-//    params.put("name", "John Doe");
-//    params.put("email", "john.doe@example.com");
-//    List<User> users = session.selectList("findUsersByNameAndEmail", params);
